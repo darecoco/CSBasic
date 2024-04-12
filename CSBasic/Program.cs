59ebbc7601bbca5da641957bc24efd74ea8d6ef0
Darecoco <whaqlrh37@naver.com>
2024-04-12 10:19:27 +0900

    #9 sizeof()로 자료형 크기 출력

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -29,6 +29,16 @@ namespace CSBasic
 
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine("===================================");
+
+            //sizeof 연산자 연습
+            Console.WriteLine("int : " + sizeof(int));
+            Console.WriteLine("long : " + sizeof(long));
+            Console.WriteLine("float : " + sizeof(float));
+            Console.WriteLine("double : " + sizeof(double));
+            Console.WriteLine("char : " + sizeof(char));
+            Console.WriteLine("bool : " + sizeof(bool));
         }
     }
 }
